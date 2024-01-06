17e9ec4be27a4a097f037a01ca1d2a922d05824a
lg-epitech <laurent.gonzalez@epitech.eu>
2024-01-06 17:17:27 +0100

    [ADD] Good practice infos

--- lua/nshare/init.lua
+++ lua/nshare/init.lua
@@ -5,14 +5,23 @@
 -- init.lua
 --
 
+local nshare = {
+    _NAME = "nshare",
+    _VERSION = "0.0.0-dev",
+    _DEPENDENCIES = {"luasocket"},
+    _LICENSE = "MIT",
+    _AUTHOR = "Laurent Gonzalez (lg-epitech)",
+}
+
 local prerequisite, lua_socket = pcall(require, "socket")
 
-print(prerequisite)
 if (not prerequisite) then
     print("ERROR LAUNCHING NSHARE\nLuaSocket isn't installed on your environment.\n\
-    Please consider checking the git repo for additional informations under 'Installation'.")
-    return
+Please consider checking the git repo for additional informations under 'Installation'.")
+    return nshare
 end
 
 local localshare = require("nshare.localshare")
 local netwshare = require("nshare.netwshare")
+
+return nshare
